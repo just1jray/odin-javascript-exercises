9d55ae6e5cbcc8dfdd4b8bc9999ae15b62aae7c8
just1jray <24859765+just1jray@users.noreply.github.com>
2024-08-30 23:20:01 -0400

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    const args = arguments;
+    let arr = args[0];
+    for (const arg in args) {
+        if (arg === '0') continue;
+        arr = arr.filter(i => i !== args[arg]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
