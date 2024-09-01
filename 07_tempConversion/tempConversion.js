35697f8d01df5c07e26c509d6de2f97d4ee3155b
just1jray <24859765+just1jray@users.noreply.github.com>
2024-09-01 16:40:49 -0400

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  return Math.round(((fTemp - 32) / 1.8) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return Math.round(((cTemp * 1.8) + 32) * 10) / 10
 };
 
 // Do not edit below this line
