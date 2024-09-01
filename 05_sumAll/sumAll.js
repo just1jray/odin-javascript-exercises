2b2255b8220aa39e49cbca1bb0bde71609850249
just1jray <24859765+just1jray@users.noreply.github.com>
2024-09-01 16:20:20 -0400

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(bottom, top) {
+    if (bottom < 0 || 
+        top < 0 ||
+        bottom % 1 != 0 ||
+        top % 1 != 0 ||
+        typeof bottom === 'string' ||
+        typeof top === 'string') {
+        return 'ERROR'
+    }
+    if (bottom > top) {
+        let placeholder = bottom;
+        bottom = top;
+        top = placeholder;
+    }
+    let total = 0;
+    for (let i = bottom; i <= top; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
