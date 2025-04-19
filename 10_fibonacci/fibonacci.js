e528507fca3c6282ee09af5828ee6f33a2d24032
just1jray <24859765+just1jray@users.noreply.github.com>
2025-04-19 18:28:41 -0400

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num === 0) return 0;
+    if (+num === 1) return 1;
+    if (+num < 0) return "OOPS";
+    return Array(+num - 2)
+        .fill(0)
+        .reduce(([a, b]) => [b, a + b], [1, 1])[1];
 };
 
 // Do not edit below this line
