c827d1127065db355916ceccf49e7719369bdd33
just1jray <24859765+just1jray@users.noreply.github.com>
2025-04-19 18:50:47 -0400

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        if (!a.yearOfDeath) { a.yearOfDeath = new Date().getFullYear()};
+        if (!b.yearOfDeath) { b.yearOfDeath = new Date().getFullYear()};
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    })[0];
 };
 
 // Do not edit below this line
