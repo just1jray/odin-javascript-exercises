5c59a23a2082f64cfeff330e11235901dc0fd648
just1jray <24859765+just1jray@users.noreply.github.com>
2025-04-19 17:56:54 -0400

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const chars = string.toLowerCase().split('');
+    const filteredChars = chars.filter(char => alphanumerical.includes(char));
+    const filteredString = filteredChars.join()
+    const reversedString = filteredChars.reverse().join();
+    if (filteredString === reversedString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
